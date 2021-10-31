1d44fdb038c811a9cbed8cf743605fa41796f496
Roman <roma.savitskiy@inbox.ru>
2021-10-31 22:47:10 +0300

    Проверка 1

--- src/main/java/com/example/tictactoe/HelloApplication.java
+++ src/main/java/com/example/tictactoe/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
+//Проверка 1
     public static void main(String[] args) {
         launch();
     }
